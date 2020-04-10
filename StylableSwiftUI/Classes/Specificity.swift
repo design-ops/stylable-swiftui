2df2d2f1ac89863310b30a037f15faa28c9856ca
Sam Dean <deanWombourne@gmail.com>
2020-04-10 09:48:19 +0100

    Namespace Specificity

--- StylableSwiftUI/Classes/Specificity.swift
+++ StylableSwiftUI/Classes/Specificity.swift
@@ -7,58 +7,61 @@
 
 import Foundation
 
-struct Specificity: Equatable, Hashable, Comparable, ExpressibleByIntegerLiteral {
+extension StylistIdentifier {
 
-    private let value: Int
+    struct Specificity: Equatable, Hashable, Comparable, ExpressibleByIntegerLiteral {
 
-    fileprivate init(value: Int) {
-        self.value = value
+        private let value: Int
+
+        fileprivate init(value: Int) {
+            self.value = value
+        }
+
+        static func < (lhs: Specificity, rhs: Specificity) -> Bool {
+            return lhs.value < rhs.value
+        }
+
+        init(integerLiteral value: Int) {
+            self.value = value
+        }
     }
 
-    static func < (lhs: Specificity, rhs: Specificity) -> Bool {
-        return lhs.value < rhs.value
-    }
+    final class SpecificityCache {
 
-    init(integerLiteral value: Int) {
-        self.value = value
-    }
-}
+        /// Store specificity values for identifiers
+        private var cache: [[StylistIdentifier.Component]: Specificity] = [:]
 
-final class SpecificityCache {
+        /// Global singleton instance of the specificty cache
+        static let shared = SpecificityCache()
 
-    /// Store specificity values for identifiers
-    private var cache: [[StylistIdentifier.Component]: Specificity] = [:]
+        /// Returns the specificity value from the array of components.
+        ///
+        /// - note: Returns the cached value if possible, otherwise stores the value in the cache
+        func specificity(for components: [StylistIdentifier.Component]) -> Specificity {
+            if let specificity = self.cache[components] {
+                //print("[SpecificityCache]", "Found score \(specificity) for \(components)")
+                return specificity
+            }
 
-    /// Global singleton instance of the specificty cache
-    static let shared = SpecificityCache()
+            let result = components.reversed().reduce((index: 0, score: 0)) { result, component in
+                var result = result
+                result.index += 1
+                if component.value != nil {
+                    result.score |= 1 << result.index
+                }
+                result.index += 1
+                if component.state != nil {
+                    result.score |= 1 << result.index
+                }
+                return result
+            }
 
-    /// Returns the specificity value from the array of components.
-    ///
-    /// - note: Returns the cached value if possible, otherwise stores the value in the cache
-    func specificity(for components: [StylistIdentifier.Component]) -> Specificity {
-        if let specificity = self.cache[components] {
-            //print("[SpecificityCache]", "Found score \(specificity) for \(components)")
+            let specificity = Specificity(value: result.score)
+
+            self.cache[components] = specificity
+            //print("[SpecificityCache]", "Stored score \(specificity) for \(components)")
+
             return specificity
         }
-
-        let result = components.reversed().reduce((index: 0, score: 0)) { result, component in
-            var result = result
-            result.index += 1
-            if component.value != nil {
-                result.score |= 1 << result.index
-            }
-            result.index += 1
-            if component.state != nil {
-                result.score |= 1 << result.index
-            }
-            return result
-        }
-
-        let specificity = Specificity(value: result.score)
-
-        self.cache[components] = specificity
-        //print("[SpecificityCache]", "Stored score \(specificity) for \(components)")
-
-        return specificity
     }
 }
